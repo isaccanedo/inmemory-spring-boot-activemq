cadd909f17e8dfcd2c8ac4ba43682bb4a97e713d
isaccanedo <isaccanedo@gmail.com>
2021-09-13 21:20:47 -0300

    Initial commit

---
+++ src/main/java/com/isaccanedo/messaging/inmemoryactivemqexample/listener/Consumer.java
@@ -0,0 +1,13 @@
+package com.isaccanedo.messaging.inmemoryactivemqexample.listener;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @JmsListener(destination = "inmemory.queue")
+    public void listener(String message) {
+        System.out.println("Received Message: " + message);
+    }
+}
---
+++ src/test/java/com/isaccanedo/messaging/inmemoryactivemqexample/InmemoryActivemqExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.isaccanedo.messaging.inmemoryactivemqexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InmemoryActivemqExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
